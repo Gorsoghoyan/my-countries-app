f59c323983f3c50e3674fe43ba717407c2477750
Gorsoghoyan <soghoyangor99@gmail.com>
2023-03-24 17:07:21 +0300

    push

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA51eHX8LSDq8TKgH7TBVO2q3DF34OMTC4",
-  authDomain: "countries-app-fa8d8.firebaseapp.com",
-  databaseURL: "https://countries-app-fa8d8-default-rtdb.firebaseio.com",
-  projectId: "countries-app-fa8d8",
-  storageBucket: "countries-app-fa8d8.appspot.com",
-  messagingSenderId: "176127806423",
-  appId: "1:176127806423:web:f62a6171ce29d85394d992",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
