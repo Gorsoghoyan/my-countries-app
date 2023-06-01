9046c30b370fab11681428e57160ba9bc9162a45
Gorsoghoyan <soghoyangor99@gmail.com>
2023-06-01 23:13:36 +0300

    push

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -13,6 +13,16 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA51eHX8LSDq8TKgH7TBVO2q3DF34OMTC4",
+//   authDomain: "countries-app-fa8d8.firebaseapp.com",
+//   databaseURL: "https://countries-app-fa8d8-default-rtdb.firebaseio.com",
+//   projectId: "countries-app-fa8d8",
+//   storageBucket: "countries-app-fa8d8.appspot.com",
+//   messagingSenderId: "176127806423",
+//   appId: "1:176127806423:web:f62a6171ce29d85394d992"
+// };
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
